1e9aff220df4f3eec22d0ae355955f7242b58be7
wangxiaoyang <wangxiaoyang@jiuqi.com.cn>
2021-01-18 16:20:08 +0800

    测试

--- src/main/java/com/wangxiaoyang/service2/client/TestService1Client.java
+++ src/main/java/com/wangxiaoyang/service2/client/TestService1Client.java
@@ -1,12 +1,16 @@
 package com.wangxiaoyang.service2.client;
 
 
+import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+@Service
 @AuthorizedFeignClient(name = "service1")
+@RequestMapping("/api")
 public interface TestService1Client {
 
-    @GetMapping(value = "/api/test/{message}")
+    @GetMapping(value = "/test/{message}")
     void test(@PathVariable(value = "message") String message);
 }
